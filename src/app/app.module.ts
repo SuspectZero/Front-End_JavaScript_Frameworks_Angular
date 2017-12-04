673649e5ce65da0797fa2956962c3e0fd2bfddb7
Tahir Awan <>
2017-12-04 15:02:42 -0500

    Basic Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {MaterialModule} from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 import 'hammerjs';
 
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {DishdetailComponent} from './dishdetail/dishdetail.component';
+
+import {DishService} from './services/dish.service';
 
 
 @NgModule({
@@ -23,7 +25,8 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
